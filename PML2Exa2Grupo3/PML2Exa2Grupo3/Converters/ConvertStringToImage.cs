5741eb04bc84dcd56a6d964db59de3708a7cf0b3
dmhernanandez <dany1999hernan@gmail.com>
2021-07-17 02:15:31 -0600

    se agrego el viewmodels y las vistas

---
+++ PML2Exa2Grupo3/PML2Exa2Grupo3/Converters/ConvertStringToImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PML2Exa2Grupo3.Converters
+{
+    public class ConvertStringToImage : IValueConverter
+
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string foto = $"{value}";
+            if (!string.IsNullOrEmpty(foto))
+            {
+
+                byte[] imgByte = Encoding.ASCII.GetBytes(foto);
+                return new MemoryStream(imgByte);
+            }
+            else
+                return new MemoryStream();
+                
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
